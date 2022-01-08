20577767f508cad0dced6d25f070709ae92c62ee
João Paulo <jpsilvaprogramador@gmail.com>
2022-01-08 18:34:04 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="assets/css/index.css" rel="stylesheet">
+</head>
+<body>
+    <div id="container">
+        
+        <header class="itemContainer">
+            <div class="itemHeader">gdfgd</div>
+            <div class="itemHeader">dfgdfg</div>
+        </header>
+
+        <main class="itemContainer">
+            <div class="itemSection">
+                <div class="boxSection">1</div>
+                <div class="boxSection">2</div>
+                <div class="boxSection">3</div>
+                <div class="boxSection">4</div>
+                <div class="boxSection">5</div>
+                <div class="boxSection">6</div>
+                <div class="boxSection">7</div>
+            </div>
+            <div class="itemSection">464</div>
+            <div class="itemSection">
+                <div class="boxSection2">1</div>
+                <div class="boxSection2">2</div>
+                <div class="boxSection2">3</div>
+                <div class="boxSection2">4</div>
+                <div class="boxSection2">5</div>
+                <div class="boxSection2">6</div>
+            </div>
+            <div class="itemSection">
+                <div class="boxSection3">1</div>
+                <div class="boxSection3">2</div>
+                <div class="boxSection3">3</div>
+            </div>
+        </main>
+
+        <footer class="itemContainer">
+            <div class="footerSection">1</div>
+            <div class="footerSection">2</div>
+            <div class="footerSection">3</div>
+            <div class="footerSection">4</div>
+        </footer>
+    </div>
+    <aside>
+        <div id="itemAside">
+            dfsfsdf
+        </div>
+    </aside>
+</body>
+</html>
